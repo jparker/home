1530599e1af1217d3e853c1debaffc9b35dfd8e6
John Parker <jparker@urgetopunt.com>
2011-02-10 16:36:49 -0800

    deploy to ash instead of kiwi

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,19 @@ set :user, 'jparker'
 set :use_sudo, false
 set :deploy_to, '/home/jparker/_public_html'
 
-set :repository, 'git@codaset.com:urgetopunt/home.git'
+set :repository, 'git@github.com:jparker/home.git'
 set :scm, :git
 set :scm_verbose, true
 set :deploy_via, :remote_cache
 set :branch, 'master'
 
-set :gateway, 'awooga@shell.speakeasy.net'
+if ENV['CAP_SSH_GATEWAY']
+  set :gateway, ENV['CAP_SSH_GATEWAY']
+end
 
-role :app, 'kiwi.urgetopunt.com'
-role :web, 'kiwi.urgetopunt.com'
-role :db,  'kiwi.urgetopunt.com', :primary => true
+role :app, 'ash.urgetopunt.com'
+role :web, 'ash.urgetopunt.com'
+role :db,  'ash.urgetopunt.com', :primary => true
 
 namespace :deploy do
   task :finalize_update do
